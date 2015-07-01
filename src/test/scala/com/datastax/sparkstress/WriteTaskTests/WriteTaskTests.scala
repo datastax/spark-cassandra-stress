068e3fb98a457c2d9fb50767cb93a6022e566486
Hope Ludwinski <hopeludwinski@HLudwinski-RMBP15.local>
2015-07-01 15:37:04 -0700

    Added tests

--- src/test/scala/com/datastax/sparkstress/WriteTaskTests/WriteTaskTests.scala
+++ src/test/scala/com/datastax/sparkstress/WriteTaskTests/WriteTaskTests.scala
@@ -6,6 +6,7 @@ import com.datastax.spark.connector.embedded.{SparkTemplate, EmbeddedCassandra}
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
+import com.datastax.sparkstress._
 
 @RunWith(classOf[JUnitRunner])
 class WriteTaskTests extends FlatSpec
@@ -20,21 +21,69 @@ class WriteTaskTests extends FlatSpec
 
   val conn = CassandraConnector(Set(EmbeddedCassandra.getHost(0)))
   conn.withSessionDo { session =>
-    session.execute("""CREATE KEYSPACE test WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 } """)
-    session.execute("""CREATE TABLE test.kv (k int PRIMARY KEY, v int)""")
-    session.execute("""INSERT INTO test.kv (k,v) VALUES (1,1)""")
-
+    session.execute(s"""CREATE KEYSPACE test1 WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 } """)
+    session.execute(s"""CREATE KEYSPACE test2 WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 } """)
+    session.execute(s"""CREATE KEYSPACE test3 WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 } """)
+    session.execute(s"""CREATE KEYSPACE test4 WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 } """)
+    session.execute(s"""CREATE KEYSPACE test5 WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 } """)
+    session.execute(s"""CREATE KEYSPACE test6 WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 } """)
   }
 
-  //TODO Replace this with real tests :)
-  "This setup" should " let me access an emedded spark context " in {
-    sc.parallelize(1 to 100).count should be (100)
+  "The RDD" should "have the correct configurations" in {
+    val config = new Config(keyspace = "test1", numPartitions = 1, totalOps = 20, numTotalKeys = 1)
+    val writer = new WriteShortRow(config, sc)
+    val rdd = writer.getRDD
+    rdd.partitions.length should be (config.numPartitions)
+    rdd.count should be (config.totalOps)
   }
 
-  it should " work with an embedded C* server " in {
-    sc.cassandraTable("test","kv").count should be (1)
-
+ "WriteShortRow" should "save correctly" in {
+    val config = new Config(keyspace = "test2", numPartitions = 1, totalOps = 6, numTotalKeys = 1)
+    val writer = new WriteShortRow(config, sc)
+    writer.setupCQL
+    writer.run
+    sc.cassandraTable(config.keyspace,config.table).count should be (6)
   }
 
+  it should "throw an exception when using bulkSave" in {
+    val config = new Config(saveMethod = "bulk")
+    val writer = new WriteShortRow(config, sc)
+    an [UnsupportedOperationException] should be thrownBy writer.run
+  }
+
+  it should "set new config options" in {
+    val config = new Config(keyspace = "test3", numPartitions = 1, totalOps = 15, numTotalKeys = 1)
+    val writer = new WriteShortRow(config, sc)
+    writer.setupCQL
+    writer.run
+    sc.cassandraTable(config.keyspace,config.table).count should be (15)
+    writer.setConfig(new Config(saveMethod = "bulk"))
+    writer.config.saveMethod should be ("bulk")
+    an [UnsupportedOperationException] should be thrownBy writer.run
+  }
+
+  "WriteWideRow" should "save correctly" in {
+    val config = new Config(testName = "writewiderow", keyspace = "test4", numPartitions = 1, totalOps = 8, numTotalKeys = 1)
+    val writer = new WriteWideRow(config, sc)
+    writer.setupCQL
+    writer.run
+    sc.cassandraTable(config.keyspace,config.table).count should be (8)
+  }
+
+  "WriteRandomWideRow" should "save correctly" in {
+    val config = new Config(testName = "writerandomwiderow", keyspace = "test5", numPartitions = 10, totalOps = 20, numTotalKeys = 1)
+    val writer = new WriteRandomWideRow(config, sc)
+    writer.setupCQL
+    writer.run
+    sc.cassandraTable(config.keyspace,config.table).count should be (20)
+  }
+
+  "WriteWideRowByPartition" should "save correctly" in {
+    val config = new Config(testName = "writewiderowbypartition", keyspace = "test6", numPartitions = 1, totalOps = 40, numTotalKeys = 5)
+    val writer = new WriteWideRowByPartition(config, sc)
+    writer.setupCQL
+    writer.run
+    sc.cassandraTable(config.keyspace,config.table).count should be (40)
+  }
 
 }
