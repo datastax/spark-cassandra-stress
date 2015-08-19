ead5a3164394941c867032957f4214b17a0fe4e7
Russell Spitzer <Russell.Spitzer@gmail.com>
2015-08-19 12:52:26 -0700

    Add in implicit for BulkSaveToC* for Connector < 1.2

--- src/connector/lessThan1.2.0/com/datastax/sparkstress/SparkStressImplicits.scala
+++ src/connector/lessThan1.2.0/com/datastax/sparkstress/SparkStressImplicits.scala
@@ -1,6 +1,11 @@
 package com.datastax.sparkstress
 
+import java.nio.file.Path
+
+import com.datastax.bdp.spark.writer.BulkWriteConf
+import com.datastax.spark.connector.{AllColumns, ColumnSelector}
 import com.datastax.spark.connector.rdd.{CassandraTableScanRDD, CassandraRDD}
+import org.apache.spark.rdd.RDD
 
 /* In these version of the connector cassandraCount did not exist and was invoked
 by calling .count */
@@ -8,7 +13,24 @@ object SparkStressImplicits {
 
   implicit def toCassandraCountable[T](rdd: CassandraTableScanRDD[T]): CassandraCountable[T] =
     new CassandraCountable(rdd)
+
+  implicit def toBulkTableWriter[T](rdd: RDD[T]): BulkTableWriter[T] =
+    new BulkTableWriter(rdd)
 }
+
+class BulkTableWriter[T](rdd: RDD[T]) {
+
+  def bulkSaveToCassandra(keyspaceName: String,
+                          tableName: String,
+                          columns: ColumnSelector = AllColumns,
+                          writeConf: BulkWriteConf = BulkWriteConf()): Unit = {
+    throw new UnsupportedOperationException
+  }
+}
+
+case class BulkWriteConf(outputDirectory: Option[Path] = None,
+                         deleteSource: Boolean = true,
+                         bufferSizeInMB: Int = 64)
 
 class CassandraCountable[T](rdd: CassandraTableScanRDD[T]) {
 
